33cce60d97d8dbd1b316f63d1bf740650bb7f9cb
me <me>
2023-01-17 13:47:37 -0800

    test server

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::net::TcpListener;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:1300")?;
+    println!("listener bound to thing");
+    println!("now listening!");
+
+    loop {
+        let (mut tcp_stream, _)  = listener.accept().unwrap();
+        let mut buf: [u8; 3] = [0u8; 3];
+        
+        while tcp_stream.read(&mut buf).unwrap() != 0 {
+            // println!("{:?}", buf);
+            let s = (buf[0] as i8, buf[1] as i8, buf[2]);
+            println!("{:?}", s);
+        }
+
+    }
+}
